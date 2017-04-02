8cd269c317b8504e650cc92739defcf08e8d4879
LiChengPeng <407156191@qq.com>
2017-04-02 23:13:01 +0800

    重构参数类

--- src/main/java/core/file/java/Parameter.java
+++ src/main/java/core/file/java/Parameter.java
@@ -2,34 +2,16 @@ package core.file.java;
 
 import core.util.TextUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 方法的参数.
  *
  * @author 李程鹏
  */
-public class Parameter {
-    /**
-     * 参数的类型
-     */
-    private Type type;
-
-    /**
-     * 参数名
-     */
-    private String name;
-
+public class Parameter extends JavaComponent {
     /**
      * 是否是不定长参数
      */
     private boolean isVariable;
-
-    /**
-     * 参数的注解
-     */
-    private List<String> annotations = new ArrayList<>();
 
     /**
      * <strong>Description:</strong>
@@ -93,6 +75,25 @@ public class Parameter {
     public String toString() {
         // 定义并初始化参数内容变量
         StringBuilder parameter = new StringBuilder();
+        // 添加注解
+        putAnnotations(parameter, null);
+        // 添加内容
+        putContent(parameter, null);
+        // 返回参数内容
+        return parameter.toString();
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 为参数放入注解(重写父类的方法).
+     * </pre>
+     *
+     * @param parameter   参数
+     * @param indentCount 缩进次数
+     */
+    @Override
+    protected void putAnnotations(StringBuilder parameter, Integer indentCount) {
         // 循环遍历参数的注解集合
         for (String annotation : annotations) {
             // 为参数添加注解
@@ -100,6 +101,19 @@ public class Parameter {
             // 添加空格
             TextUtils.addSpace(parameter);
         }
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 为参数添加内容.
+     * </pre>
+     *
+     * @param parameter   参数
+     * @param indentCount 缩进次数
+     */
+    @Override
+    protected void putContent(StringBuilder parameter, Integer indentCount) {
         // 添加参数的类型
         parameter.append(type.getTypeName());
         // 添加空格
@@ -113,7 +127,11 @@ public class Parameter {
         }
         // 添加参数名字
         parameter.append(name);
-        // 返回参数内容
-        return parameter.toString();
     }
+
+    /**
+     * 空方法
+     */
+    @Override
+    protected void putKeyWords(StringBuilder javaElement) {}
 }
