5818acbd7ba35dbaec538a9155d30e4aa83469f2
LiChengPeng <407156191@qq.com>
2017-04-02 22:37:42 +0800

    重构Field类

--- src/main/java/core/file/java/Field.java
+++ src/main/java/core/file/java/Field.java
@@ -2,157 +2,28 @@ package core.file.java;
 
 import core.util.TextUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 属性.
  *
  * @author 李程鹏
  */
-public class Field {
-    /**
-     * 权限控制符
-     */
-    private String visibility;
-
-    /**
-     * 是否是static属性
-     */
-    private boolean isStatic;
-
-    /**
-     * 是否是final属性
-     */
-    private boolean isFinal;
-
-    /**
-     * 属性类型
-     */
-    private Type type;
-
-    /**
-     * 属性名
-     */
-    private String name;
-
+public class Field extends JavaComponent {
     /**
      * 初始值
      */
     private String initValue;
 
     /**
-     * 文档注释
-     */
-    private List<String> documents = new ArrayList<>();
-
-    /**
-     * 注解
-     */
-    private List<String> annotations = new ArrayList<>();
-
-    /**
      * <strong>Description:</strong>
      * <pre>
-     * 设置权限控制符.
+     * 设置初始值.
      * </pre>
      *
-     * @param visibility 权限控制符
-     */
-    public void setVisibility(String visibility) {
-        // 赋值
-        this.visibility = visibility;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置static属性标志.
-     * </pre>
-     *
-     * @param isStatic static属性标志
-     */
-    public void setStatic(boolean isStatic) {
-        // 赋值
-        this.isStatic = isStatic;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置final属性标志.
-     * </pre>
-     *
-     * @param isFinal final属性标志
-     */
-    public void setFinal(boolean isFinal) {
-        // 赋值
-        this.isFinal = isFinal;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置属性类型.
-     * </pre>
-     *
-     * @param type 属性类型
-     */
-    public void setType(Type type) {
-        // 赋值
-        this.type = type;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置属性名.
-     * </pre>
-     *
-     * @param name 属性名
-     */
-    public void setName(String name) {
-        // 赋值
-        this.name = name;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置初始化值.
-     * </pre>
-     *
-     * @param initValue 初始化值
+     * @param initValue 初始值
      */
     public void setInitValue(String initValue) {
         // 赋值
         this.initValue = initValue;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 添加文档注释.
-     * </pre>
-     *
-     * @param document 文档注释
-     */
-    public void addDocument(String document) {
-        // 将文档注释添加到注释集合中
-        documents.add(document);
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 添加注解.
-     * </pre>
-     *
-     * @param annotation 注解
-     */
-    public void addAnnotation(String annotation) {
-        // 将注解添加到注解集合中
-        annotations.add(annotation);
     }
 
     /**
@@ -167,42 +38,49 @@ public class Field {
     public String toString(int indentCount) {
         // 定义并初始化属性内容变量
         StringBuilder field = new StringBuilder();
-        // 遍历文档注释集合
-        for (String document : documents) {
-            // 缩进
-            TextUtils.addIndentation(field, indentCount);
-            // 添加文档内容
-            field.append(document);
-            // 换行
-            TextUtils.addLine(field);
-        }
-        // 遍历注解集合
-        for (String annotation : annotations) {
-            // 缩进
-            TextUtils.addIndentation(field, indentCount);
-            // 添加注解
-            field.append(annotation);
-            // 换行
-            TextUtils.addLine(field);
-        }
+        // 添加文档注释
+        putDocuments(field, indentCount);
+        // 添加注解
+        putAnnotations(field, indentCount);
         // 缩进
         TextUtils.addIndentation(field, indentCount);
-        // 添加权限控制符
-        field.append(visibility);
-        // 如果是static类型的属性
-        if (isStatic) {
-            // 添加static关键字
-            field.append("static");
-            // 添加空格
-            TextUtils.addSpace(field);
-        }
-        // 如果是final类型的属性
-        if (isFinal) {
-            // 添加final关键字
-            field.append("final");
-            // 添加空格
-            TextUtils.addSpace(field);
-        }
+        // 添加关键字
+        putKeyWords(field);
+        // 添加内容
+        putContent(field, null);
+        // 返回结果
+        return field.toString();
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 添加关键字.
+     * </pre>
+     *
+     * @param field 属性
+     */
+    @Override
+    protected void putKeyWords(StringBuilder field) {
+        // 添加访问控制符
+        putVisibility(field);
+        // 添加静态标志
+        putStatic(field);
+        // 添加终结标志
+        putFinal(field);
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 添加内容.
+     * </pre>
+     *
+     * @param field       属性
+     * @param indentCount 缩进次数
+     */
+    @Override
+    protected void putContent(StringBuilder field, Integer indentCount) {
         // 添加属性类型
         field.append(type.getTypeName());
         // 添加空格
@@ -222,7 +100,5 @@ public class Field {
         }
         // 添加分号
         field.append(';');
-        // 返回结果
-        return field.toString();
     }
 }
