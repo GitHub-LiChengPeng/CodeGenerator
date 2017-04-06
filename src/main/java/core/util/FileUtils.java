22f9871757d2fdccc945f93d2d79193ba85a0e59
SCAL <407156191@qq.con>
2017-04-06 15:23:26 +0800

    添加文件操作工具类

---
+++ src/main/java/core/util/FileUtils.java
@@ -0,0 +1,53 @@
+package core.util;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+/**
+ * 文件操作工具类.
+ *
+ * @author 李程鹏
+ */
+public class FileUtils {
+    /**
+     * 本机的桌面路径
+     */
+    private static String desktop = FileSystemView.getFileSystemView().getHomeDirectory().getPath();
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 生成文件.
+     * </pre>
+     *
+     * @param relativePath 相对路径
+     * @param fileName     文件名
+     * @param content      文件内容
+     */
+    public static void generateFile(String relativePath, String fileName, String content) throws Exception {
+        // 拼接出文件的绝对路径
+        String absolutePath = desktop + relativePath;
+        // 定义文件路径对象
+        File directory = new File(absolutePath);
+        // 如果路径不存在
+        if (!directory.exists()) {
+            // 创建路径
+            directory.mkdirs();
+        }
+        // 定义文件对象
+        File file = new File(absolutePath + fileName);
+        // 根据文件对象创建文件输出流
+        FileOutputStream fileOutputStream = new FileOutputStream(file, false);
+        // 创建输出流写入器
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "utf-8");
+        // 创建缓冲区写入器
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        // 写入文件内容
+        bufferedWriter.write(content);
+        // 关闭缓冲区写入器
+        bufferedWriter.close();
+    }
+}
