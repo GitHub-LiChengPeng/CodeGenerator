0f1b2697f8d7d8af3e97a0eea6eca6deef74753d
SCAL <407156191@qq.con>
2017-04-06 15:02:09 +0800

    更新Main函数

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -2,8 +2,6 @@ package core;
 
 import core.database.DatabaseReader;
 import core.database.Table;
-import core.file.java.Interface;
-import core.file.java.Class;
 import core.generator.dao.*;
 
 import java.util.List;
@@ -13,7 +11,6 @@ import java.util.List;
  *
  * @author 李程鹏
  */
-
 public class Main {
     /**
      * <strong>Description:</strong>
@@ -24,16 +21,9 @@ public class Main {
      * @param args 参数
      */
     public static void main(String[] args) throws Exception {
+        // 读取数据库表格
         List<Table> tables = new DatabaseReader("root", "root", "test").readTables();
-
-        POGenerator poGenerator = new POGenerator(tables.get(0));
-        Class class_ = poGenerator.generate();
-        System.out.println(class_.toString(0));
-
-        System.out.println("--------------------------------------------");
-
-        DaoGenerator daoGenerator = new DaoGenerator(tables.get(0));
-        Interface interface_ = daoGenerator.generate();
-        System.out.println(interface_.toString());
+        // 生成Dao层代码(Hibernate).
+        new HibernateGenerator(tables).generate();
     }
 }
