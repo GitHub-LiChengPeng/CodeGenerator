f43ab4424c5700c1a1bfab2eefcf46484775c61a
LiChengPeng <407156191@qq.com>
2017-04-09 16:28:04 +0800

    重构代码

--- src/main/java/core/generator/ClassGenerator.java
+++ src/main/java/core/generator/ClassGenerator.java
@@ -2,10 +2,7 @@ package core.generator;
 
 import core.file.java.Class;
 import core.database.Table;
-import core.file.java.Method;
-import core.file.java.Parameter;
 import core.file.java.Type;
-import core.util.PackageUtils;
 import core.util.StringUtils;
 
 /**
@@ -25,11 +22,6 @@ public abstract class ClassGenerator extends Generator {
     protected Type interfaceType;
 
     /**
-     * 实现类需要操作的实体类型
-     */
-    protected Type entityType;
-
-    /**
      * 需要操作的实体类型名
      */
     protected String entityTypeName;
@@ -45,8 +37,6 @@ public abstract class ClassGenerator extends Generator {
     public ClassGenerator(Table table) {
         // 调用父类的构造方法
         super(table);
-        // 初始化实体类型
-        this.entityType = new Type(PackageUtils.ENTITY.getValue() + getTableName());
         // 初始化实体类型名
         this.entityTypeName = entityType.getTypeName();
     }
@@ -78,104 +68,5 @@ public abstract class ClassGenerator extends Generator {
         class_.addImport(new Type("org.springframework.beans.factory.annotation.*"));
         // 导入的这个包下包含@Controller,@Service和@Repository
         class_.addImport(new Type("org.springframework.stereotype.*"));
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 放入增加方法的基本信息.
-     * </pre>
-     *
-     * @param method 方法
-     */
-    protected void putAddMethodInfo(Method method) {
-        // 设置访问控制符
-        method.setVisibility("public ");
-        // 设置方法名
-        method.setName("addEntity");
-        // 新建一个参数
-        Parameter parameter = new Parameter(entityType, "entity");
-        // 为方法添加参数
-        method.addParameter(parameter);
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 放入删除方法的基本信息.
-     * </pre>
-     *
-     * @param method 方法
-     */
-    protected void putDeleteMethodInfo(Method method) {
-        // 设置访问控制符
-        method.setVisibility("public ");
-        // 设置方法名
-        method.setName("deleteEntity");
-        // 新建一个参数
-        Parameter parameter = new Parameter(new Type("int"), "id");
-        // 为方法添加参数
-        method.addParameter(parameter);
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 放入修改方法的基本信息.
-     * </pre>
-     *
-     * @param method 方法
-     */
-    protected void putUpdateMethodInfo(Method method) {
-        // 设置访问控制符
-        method.setVisibility("public ");
-        // 设置方法名
-        method.setName("updateEntity");
-        // 新建一个参数
-        Parameter parameter = new Parameter(entityType, "entity");
-        // 为方法添加参数
-        method.addParameter(parameter);
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 放入查询方法(按主键查)的基本信息.
-     * </pre>
-     *
-     * @param method 方法
-     */
-    protected void putReadOneMethodInfo(Method method) {
-        // 设置访问控制符
-        method.setVisibility("public ");
-        // 设置方法名
-        method.setName("readEntity");
-        // 设置方法的返回值类型
-        method.setType(entityType);
-        // 新建一个参数
-        Parameter parameter = new Parameter(new Type("int"), "id");
-        // 为方法添加参数
-        method.addParameter(parameter);
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 放入查询方法(查询所有)的基本信息.
-     * </pre>
-     *
-     * @param method 方法
-     */
-    protected void putReadAllMethodInfo(Method method) {
-        // 设置访问控制符
-        method.setVisibility("public ");
-        // 设置方法名
-        method.setName("readEntities");
-        // 定义方法返回值类型
-        Type returnType = new Type("java.util.List");
-        // 设置返回值类型中的泛型参数
-        returnType.addTypeArgument(entityType);
-        // 为方法设置返回值类型
-        method.setType(returnType);
     }
 }
