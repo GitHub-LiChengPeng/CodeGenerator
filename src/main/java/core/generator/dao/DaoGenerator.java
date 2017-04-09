2e93807e11e0231d9b6b323f1293ee5cede7b9ff
LiChengPeng <407156191@qq.com>
2017-04-09 16:41:54 +0800

    为Dao层接口生成接口文档注释

--- src/main/java/core/generator/dao/DaoGenerator.java
+++ src/main/java/core/generator/dao/DaoGenerator.java
@@ -17,12 +17,14 @@ public class DaoGenerator extends InterfaceGenerator {
      * 构造实例化生成器.
      * </pre>
      *
-     * @param
+     * @param table 表格对象
      */
     public DaoGenerator(Table table) {
         // 调用父类的构造方法
         super(table);
         // 实例化Dao层接口的类型
         this.interfaceType = new Type(PackageUtils.DAO.getValue() + getTableName() + "Dao");
+        // 设置Dao接口的文档注释
+        this.comment = "持久层接口";
     }
 }
