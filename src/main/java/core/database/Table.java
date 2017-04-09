4d39c61f76ee934d38afb9a236cc864f23cf21d1
LiChengPeng <407156191@qq.com>
2017-04-09 14:11:04 +0800

    添加注释属性然后重构代码

--- src/main/java/core/database/Table.java
+++ src/main/java/core/database/Table.java
@@ -7,42 +7,11 @@ import java.util.List;
  *
  * @author 李程鹏
  */
-public class Table {
-    /**
-     * 表名
-     */
-    private String name;
-
+public class Table extends DatabaseElement {
     /**
      * 表中的列
      */
     private List<Column> columns;
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 获取表名.
-     * </pre>
-     *
-     * @return {@code java.lang.String} - 表名
-     */
-    public String getName() {
-        // 返回表名
-        return name;
-    }
-
-    /**
-     * <strong>Description:</strong>
-     * <pre>
-     * 设置表名.
-     * </pre>
-     *
-     * @param name 表名
-     */
-    public void setName(String name) {
-        // 赋值
-        this.name = name;
-    }
 
     /**
      * <strong>Description:</strong>
