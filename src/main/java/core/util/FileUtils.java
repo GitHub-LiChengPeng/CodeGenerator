abfed61394a4268075943d44dfafad8ae0eb3afc
LiChengPeng <407156191@qq.com>
2017-04-11 00:40:52 +0800

    添加了拷贝文件和文件夹的方法

--- src/main/java/core/util/FileUtils.java
+++ src/main/java/core/util/FileUtils.java
@@ -1,9 +1,6 @@
 package core.util;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 /**
  * 文件操作工具类.
@@ -45,5 +42,83 @@ public class FileUtils {
         bufferedWriter.write(content);
         // 关闭缓冲区写入器
         bufferedWriter.close();
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 拷贝文件.
+     * </pre>
+     *
+     * @param from 来源文件
+     * @param to   目的文件
+     */
+    public static void copyFile(String from, String to) {
+        // 定义缓冲读取器
+        BufferedReader bufferedReader = null;
+        // 定义打印流对象
+        PrintStream printStream = null;
+        try {
+            // 初始化缓冲去读器
+            bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(from)));
+            // 初始化打印流对象
+            printStream = new PrintStream(new FileOutputStream(to));
+            // 定义行内容变量
+            String line;
+            // 读取数据
+            while ((line = bufferedReader.readLine()) != null) {
+                // 将读取到的内容打印到目标文件中
+                printStream.println(line);
+                // 刷新打印流对象
+                printStream.flush();
+            }
+        } catch (Exception e) {
+            // 如果出了异常,打印异常信息.
+            System.out.println("拷贝文件出错!");
+        } finally {
+            try {
+                // 关闭缓冲读取器
+                if (bufferedReader != null) bufferedReader.close();
+                // 关闭打印流对象
+                if (printStream != null) printStream.close();
+            } catch (IOException e) {
+                // 如果出了异常,打印异常信息.
+                System.out.println("关闭IO对象出错!");
+            }
+        }
+    }
+
+    /**
+     * <strong>Description:</strong>
+     * <pre>
+     * 拷贝文件夹.
+     * </pre>
+     *
+     * @param from 来源路径
+     * @param to   目的路径
+     */
+    public static void copyDirectory(String from, String to) {
+        // 根据来源路径新建一个文件夹对象
+        File sourceDirectory = new File(from);
+        // 获取来源路径下的所有文件和文件夹
+        File[] sourceFiles = sourceDirectory.listFiles();
+        // 根据目的路径新建一个文件夹对象
+        File destinationDirectory = new File(to);
+        // 如果目的路径不存在
+        if (!destinationDirectory.exists()) {
+            // 创建目的路径
+            destinationDirectory.mkdirs();
+        }
+        // 遍历来源路径下的文件和文件夹
+        for (File file : sourceFiles) {
+            // 如果是文件
+            if (file.isFile()) {
+                // 拷贝文件到指定目录下
+                copyFile(file.getPath(), to + "\\" + file.getName());
+            } else if (file.isDirectory()) {// 如果是文件夹
+                // 递归调用拷贝文件夹的方法
+                copyDirectory(file.getPath(), to + "\\" + file.getName());
+            }
+        }
     }
 }
